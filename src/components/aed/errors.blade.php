2dafda0250e40768e6e3bba1187973acd9df58a2
Abelardo Recalde <j26a90@gmail.com>
2023-09-25 14:29:47 +0300

    errors component

--- src/components/aed/errors.blade.php
+++ src/components/aed/errors.blade.php
@@ -1,7 +1,7 @@
 @props(['errors'])
 
 @if ($errors->any())
-    <div {{ $attributes->merge(['class' => 'app-validation']) }}>
+    <div {{ $attributes->merge(['class' => 'app-validation border-in-error']) }}>
         @foreach ($errors->all() as $error)
             <p class="m-0-0"><small>{{ $error }}</small></p>
         @endforeach
